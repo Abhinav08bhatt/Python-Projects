e11337f60ca2dd2dab6fb03717adcf77ee8e0fca
Abhinav08bhatt <abhinav08bhatt@gmail.com>
2026-04-17 07:45:00 +0530

    learning opps 02

--- OOPS/DOCS/note_01.md
+++ OOPS/DOCS/note_01.md
@@ -1,2 +1,17 @@
 # Classes
 > classes are the blueprint for our code
+
+```py
+class ClassName:
+    # class content
+    property1 = 00
+
+    def method1(self, -- , -- ,):
+        # method content
+
+class_obj = ClassName
+class_obj.property1
+class_obj.method1()
+```
+
+> object is an instance of the class (an example of a class)
--- OOPS/file_01_intro.py
+++ OOPS/file_01_intro.py
@@ -1,19 +1,32 @@
 class Dog:
 
-    def __init__(self, name, breed):
+    def __init__(self, name, breed, owner):
         self.name_of_dog = name 
-        self.breed__of_dog = breed 
+        self.breed_of_dog = breed 
+        self.owner_of_dog = owner 
 
 
     def bark(self):
         print("woof woof")
 
-dog1 = Dog("bruse","german")
+class Owner:
+    def __init__(self, name, address, mobile_num):
+        self.name = name
+        self.address = address
+        self.mobile_num = mobile_num
+
+
+owner1 = Owner("richard","123 street","2435632")
+
+dog1 = Dog("bruse","german",owner1)
 dog1.bark()
 print(dog1.name_of_dog)
-print(dog1.breed__of_dog)
+print(dog1.breed_of_dog)
+print(dog1.owner_of_dog.name)
 
-dog2 = Dog("allly","scotich")
+owner2 = Owner("sally","123 street","2435632")
+
+dog2 = Dog("allly","scotich",owner2)
 dog2.bark()
 print(dog2.name_of_dog)
-print(dog2.breed__of_dog)
+print(dog2.breed_of_dog)
